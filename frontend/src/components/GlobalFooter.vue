bcc3b5ed34b242adee7ed7f012912c3088a149a7
apprentice217 <15036702468@163.com>
2026-04-14 20:50:35 +0800

    前端页面的一些文字性修改

--- frontend/src/components/GlobalFooter.vue
+++ frontend/src/components/GlobalFooter.vue
@@ -2,14 +2,7 @@
   <a-layout-footer class="footer">
     <div class="footer-content">
       <p class="copyright">
-        <a
-          href="https://www.codefather.cn"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="author-link"
-        >
-          编程导航原创项目
-        </a>
+        © 2024 AI 网关
       </p>
     </div>
   </a-layout-footer>
